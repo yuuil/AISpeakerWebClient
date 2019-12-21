319ddcb021edfc18cc1b483842e37760f029436e
KLumy <lunox273@gmail.com>
2019-12-21 19:47:24 +0900

    서울날씨

--- static/js/main.js
+++ static/js/main.js
@@ -2,5 +2,7 @@ $(document).ready(function() {
   let apiGatewat = new APIGateway("서버이름");
   let controller = new Controller();
   let model = new Model();
+  let view = new View();
   let sttManager = new STTManger();
+  let ttsManager = new TTSModel();
 });
