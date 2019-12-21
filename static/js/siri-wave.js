94d42f00bf3dee58f44b162dfa03b498cc4a513d
Yewon Jeong <yeyeah_1@daum.net>
2019-12-21 19:45:45 +0900

    add control icon

--- static/js/siri-wave.js
+++ static/js/siri-wave.js
@@ -16,14 +16,12 @@ function randomAmplitude() {
 
 randomAmplitude();
 
-window.onload = function() {
-  //SiriWave는 이미 만들어진 클래스이기 때문에 너비를 임의로 제어하기 위해 onload 이벤트 사용.
-  const siri = document.querySelector('#siri-wave > canvas');
-  siri.style.width = "100%";
-}
-
 $(function() {
-  $("#speaker-circle, #siri-wave").click(function() {
+  //SiriWave는 이미 만들어진 클래스이기 때문에 너비를 임의로 제어하기 위해 onload 이벤트 사용.
+  const siri = $("#siri-wave > canvas");
+  siri.css({"width":"100%"});
+  
+  $(".core-voice, #siri-wave").click(function() {
     if($("#speaker-circle").hasClass("on")) {
       console.log("Speaker Off");
       $("#speaker-circle").removeClass("on");
